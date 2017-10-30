cbec3d7c57a3a86a3f06be70b8686f6cae403b73
goodevil_git <rolandas.valantinas@livebuzz.co.uk>
2017-10-30 20:29:37 +0000

    WIP

--- facebook-marketplace.hour.user.js
+++ facebook-marketplace.hour.user.js
@@ -6,15 +6,27 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.1
+// @version      1.2
 // ==/UserScript==
 
 (function () {
-    setInterval(removeItem, 2000);
+    var interval = setInterval(removeItem, 3000);
+    var attempts = 0;
 
     function removeItem() {
-        $('._1oem')
-            .has('._uc9:contains("over a week ago"), ._uc9:contains("days ago"), ._uc9:contains("day ago"), ._uc9:contains("hours ago")')
-            .remove();
+        var items = $('._1oem')
+            .has('._uc9:contains("over a week ago"), ._uc9:contains("days ago"), ._uc9:contains("day ago"), ._uc9:contains("hours ago")');
+
+        if (items.length > 0) {
+            attempts = 0;
+            items.remove();
+            window.scrollTo(0, $(document).height());
+        } else {
+            attempts++;
+        }
+
+        if (attempts > 5) {
+            clearInterval(interval);
+        }
     }
 })();
--- facebook-marketplace.week.user.js
+++ facebook-marketplace.week.user.js
@@ -6,13 +6,27 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.0
+// @version      1.1
 // ==/UserScript==
 
 (function () {
-    setInterval(removeItem, 2000);
+    var interval = setInterval(removeItem, 3000);
+    var attempts = 0;
 
     function removeItem() {
-        $('._1oem').has('._uc9:contains("over a week ago")').remove();
+        var items = $('._1oem')
+            .has('._uc9:contains("over a week ago")');
+
+        if (items.length > 0) {
+            attempts = 0;
+            items.remove();
+            window.scrollTo(0, $(document).height());
+        } else {
+            attempts++;
+        }
+
+        if (attempts > 5) {
+            clearInterval(interval);
+        }
     }
 })();
