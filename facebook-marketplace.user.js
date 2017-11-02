25bdba650d64eed5d52de4d18ea089c5fd6ae244
goodevil_git <rolandas.valantinas@livebuzz.co.uk>
2017-11-02 10:12:45 +0000

    WIP

--- facebook-marketplace.user.js
+++ facebook-marketplace.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @author       Rolandas Valantinas
-// @description  remove entries posted more than one day ago
+// @description  remove entries posted more 23 hours ago
 // @include      *facebook.com/marketplace*
 // @exclude      *facebook.com/marketplace/buying
 // @exclude      *facebook.com/marketplace/saved
@@ -9,7 +9,7 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.3
+// @version      1.4
 // ==/UserScript==
 
 (function () {
@@ -18,7 +18,7 @@
 
     function removeItem() {
         var items = $('._1oem')
-            .has('._uc9:contains("over a week ago"), ._uc9:contains("days ago")');
+            .has('._uc9:contains("over a week ago"), ._uc9:contains("days ago"), ._uc9:contains("day ago")');
 
         if (items.length > 0) {
             attempts = 0;
