4fdb54812ba6d6ceb79e64665812f9ea8cc4ffa2
Rolandas Valantinas <pixonisltd@gmail.com>
2019-12-15 15:56:48 +0000

    Update for new eBay look

--- ebay-hide-range-listings.user.js
+++ ebay-hide-range-listings.user.js
@@ -6,17 +6,17 @@
 // @name         Hide ebay range listings
 // @namespace    https://greasyfork.org/users/157178
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.0
+// @version      1.1
 // ==/UserScript==
 
 (function () {
     var interval = setInterval(hideListings, 2000);
 
     function hideListings() {
-        var price = document.getElementsByClassName("prRange");
+        var price = document.getElementsByClassName("DEFAULT");
 
         for (var i = 0; i < price.length; i++) {
-            price[i].parentNode.parentNode.parentNode.parentNode.remove();
+            price[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
         }
 
         if (price.length === 0) {
