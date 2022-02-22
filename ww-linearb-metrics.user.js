a1674fa66967f2d4b57107dd69db22644a22554f
Rolandas Valantinas <pixonisltd@gmail.com>
2022-02-22 10:28:17 +0000

    Update after HTML has changed

--- ww-linearb-metrics.user.js
+++ ww-linearb-metrics.user.js
@@ -6,7 +6,7 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.0
+// @version      1.1
 // ==/UserScript==
 
 (function () {
@@ -14,23 +14,27 @@
     var intervalButton = setInterval(addButton, 2000);
     var attempts = 0;
 
+    function appendData(title, value) {
+        $("p:contains('"+title+"')").first().parent().siblings().children().first().children().first().append(value);
+    };
+
     function addMetrics() {
         console.log('add addMetrics');
 
-        $("p:contains('CODING TIME')").first().parent().siblings().children().first().append(' / <24h');
-        $("p:contains('PICKUP TIME')").first().parent().siblings().children().first().append(' / <12h');
-        $("p:contains('REVIEW TIME')").first().parent().siblings().children().first().append(' / <4h');
-        $("p:contains('PRS OPENED')").first().parent().siblings().children().first().append(' / >2 per day');
-        $("p:contains('PR SIZE')").first().parent().siblings().children().first().append(' / <500');
-        $("p:contains('REVIEW DEPTH')").first().parent().siblings().children().first().append(' / 2-4');
-        $("p:contains('NEW CODE')").first().parent().siblings().children().first().append(' / >70%');
-        $("p:contains('REFACTOR')").first().parent().siblings().children().first().append(' / <25%');
-        $("p:contains('REWORK')").first().parent().siblings().children().first().append(' / <5%');
+        appendData('CODING TIME', ' / <24h');
+        appendData('PICKUP TIME', ' / <12h');
+        appendData('REVIEW TIME', ' / <4h');
+        appendData('PRS OPENED', ' / <500');
+        appendData('PR SIZE', ' / <500');
+        appendData('REVIEW DEPTH', ' / 2-4');
+        appendData('NEW CODE', ' / >70%');
+        appendData('REFACTOR', ' / <25%');
+        appendData('REWORK', ' / <5%');
 
-        $("p:contains('CYCLE TIME')").first().parent().siblings().children().first().append(' / <2 days');
-        $("p:contains('DEPLOY FREQUENCY')").first().parent().siblings().children().first().append(' / >1 per day');
-        $("p:contains('DEPLOY TIME')").first().parent().siblings().children().first().append(' / <6h');
-        $("p:contains('MTTR')").first().parent().siblings().children().first().append(' / <4h');
+        appendData('CYCLE TIME', ' / <2 days');
+        appendData('DEPLOY FREQUENCY', ' / >1 per day');
+        appendData('DEPLOY TIME', ' / <6h');
+        appendData('MTTR', ' / <4h');
     }
 
 
