4c4baf86a55f456c1e712885f9c432f41a5d900f
Rolandas Valantinas <pixonisltd@gmail.com>
2022-06-16 14:53:21 +0100

    bump version

--- ww-linearb-metrics.user.js
+++ ww-linearb-metrics.user.js
@@ -6,7 +6,7 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.2
+// @version      1.3
 // ==/UserScript==
 
 (function () {
