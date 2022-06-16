69288564748f1f478583e2c435ef7250d3f3f249
Rolandas Valantinas <pixonisltd@gmail.com>
2022-06-16 15:38:36 +0100

    fix bug in linearb script

--- ww-linearb-metrics.user.js
+++ ww-linearb-metrics.user.js
@@ -6,7 +6,7 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.3
+// @version      1.4
 // ==/UserScript==
 
 (function () {
@@ -15,7 +15,7 @@
     var attempts = 0;
 
     function appendData(title, value) {
-        $("p:contains('"+title+"')").first().parent().parent().find("p").filter(":nth-child(1)").append(value);
+        $("p:contains('"+title+"')").first().parent().parent().find("p")[1].append(value);
     };
 
     function addMetrics() {
