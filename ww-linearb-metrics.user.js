d44be58d2d5da9814f54346cfb79c8e3768b3279
rolandas-valantinas <rolandas.valantinas@wealthwizards.com>
2022-07-21 10:57:00 +0100

    linearB: use new selectors, add button to different location

--- ww-linearb-metrics.user.js
+++ ww-linearb-metrics.user.js
@@ -6,7 +6,7 @@
 // @namespace    https://greasyfork.org/users/157178
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
 // @supportURL   https://github.com/rolandas-valantinas/gists/issues
-// @version      1.5
+// @version      1.6
 // ==/UserScript==
 
 (function () {
@@ -15,38 +15,40 @@
     var attempts = 0;
 
     function appendData(title, value) {
-        $("p:contains('"+title+"')").first().parent().parent().find("p").eq(1).append(value);
+        $("p[data-cy='"+title+"']").append(value);
     };
 
     function addMetrics() {
         console.log('add addMetrics');
 
-        appendData('CODING TIME', ' / <24h');
-        appendData('PICKUP TIME', ' / <12h');
-        appendData('REVIEW TIME', ' / <4h');
-        appendData('PRS OPENED', ' / >2 per day');
-        appendData('PR SIZE', ' / <500');
-        appendData('REVIEW DEPTH', ' / 2-4');
-        appendData('NEW CODE', ' / >70%');
-        appendData('REFACTOR', ' / <25%');
-        appendData('REWORK', ' / <5%');
+        appendData('coding-time-graph-value', ' / <24h');
+        appendData('pickup-time-graph-value', ' / <12h');
+        appendData('review-time-graph-value', ' / <4h');
+        appendData('prs-opened-graph-value', ' / >2 per day');
+        appendData('pr-size-graph-value', ' / <500');
+        appendData('review-depth-graph-value', ' / 2-4');
+        appendData('new-code-graph-value', ' / >70%');
+        appendData('refactor-graph-value', ' / <25%');
+        appendData('rework-graph-value', ' / <5%');
 
-        appendData('CYCLE TIME', ' / <2 days');
-        appendData('DEPLOY FREQUENCY', ' / >1 per day');
-        appendData('DEPLOY TIME', ' / <6h');
-        appendData('MTTR', ' / <4h');
+        appendData('cycle-time-graph-value', ' / <2 days');
+        appendData('deploy-frequency-graph-value', ' / >1 per day');
+        appendData('deploy-time-graph-value', ' / <6h');
+        appendData('mttr-graph-value', ' / <4h');
     }
 
 
     function addButton() {
         console.log('add button');
         attempts++;
-        var toolbar = $('div.MuiInputBase-root');
+        var toolbar = $("div[data-cy='Dashboard-data-cy']");
+        console.log(toolbar);
 
         if (toolbar.length > 0) {
-            toolbar.parent().parent().append(metricsButton);
+            toolbar.append(metricsButton);
             $('#add-ww-metrics').click(addMetrics);
             attempts = 10;
+            console.log('button added');
         }
 
         if (attempts > 5) {
